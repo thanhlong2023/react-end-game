d99335b2a422154eff741e5b0bb2e9c5a261f2ff
thanhlong2023 <caolong770709@gmail.com>
2024-04-17 13:00:07 +0700

    fadsf

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+//class component
+//function component
+import React from "react"
+
+class MyComponent extends React.Component {
+
+    // JSX 
+    render() {
+        return (
+            <div>My first component</div>
+        )
+    }
+}
+
+export default MyComponent
